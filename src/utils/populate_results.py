f3550ac8eea7ec39bb7476e0b32abeec548b7a30
Piergiacomo De Marchi <piergiacomo.demarchi@gmail.com>
2020-03-29 18:54:20 +0200

    improved JSON structuret of processed data to feed UI

--- src/utils/populate_results.py
+++ src/utils/populate_results.py
@@ -9,14 +9,28 @@ import json
 '''
 # example of input data
 predictions_dict = {
-   "results":[
-      {
-         "country_code": "IT",
-         "number_cases": "40000"
-      },
-      {
-         "country_code": "Spain",
-         "number_cases": "30000"
+  "results":[
+    {
+      "country_code": "IT",
+      "country_name": "Italy",
+      "resources_capacity": "39000",
+      "confirmed": "40000",
+      "deaths": "3000",
+      "recovered": "800",
+      "confirmed_prediction_3w": "50000",
+      "deaths_prediction_3w": "4000",
+      "recovered_prediction_3w": "900"
+    },
+    {
+      "country_code": "ES",
+      "country_name": "Spain",
+      "resources_capacity": "40000",
+      "confirmed": "30000",
+      "deaths": "2500",
+      "recovered": "600",
+      "confirmed_prediction_3w": "60000",
+      "deaths_prediction_3w": "2800",
+      "recovered_prediction_3w": "1200"
       }
    ],
    "timestamp": "2020-03-29 17:05:51.514470"
